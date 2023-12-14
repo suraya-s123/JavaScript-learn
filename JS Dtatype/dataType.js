43306a7a1b2b76062e98bcae683c9fbeac4f0c76
suraya-s123 <sarminsuraya@gmail.com>
2023-12-14 12:43:09 +0600

    Dtatype Revise

--- JS Dtatype/dataType.js
+++ JS Dtatype/dataType.js
@@ -1,2 +1,64 @@
-let x= 16 +" "+ "Volvo";
-document.getElementById("demo").innerHTML= x;
+let a = 16 + " VolVo ";
+console.log(a);
+let b = 16+ 4 + "Volvo";
+console.log(b);
+let c = "volvo" + 16 + 4;
+console.log(c);
+
+// Type are dynamic
+
+let x;
+x = 5;
+x= "Volvo";
+x= 34
+console.log(x)
+
+// JavaScript Strings
+let string1 =" This a string  Value of string1 using the double quot";
+ console.log(string1);
+let string2='This a string value of string2 using the single quot';
+console.log(string2);
+
+// JavaScript Numbers
+
+let num1= 34.00; // o.p = 34
+console.log(num1);  // o.p = 34
+let num2= 34;
+console.log(num2);
+
+// Exponential Notation
+let y= 123e5; // 12300000
+console.log(y);
+let z= 123e-5; // o.p =0.00123
+console.log(z);
+
+//JavaScript Booleans
+x= 5;
+y = 5;
+z= 3;
+console.log(x==y); // true, for 5= 5
+console.log(y==z) // false, for  5!=3
+
+// Javascript Array
+ let cars=[ "Volvo", "Saab", "BMW" ];
+ console.log(cars);
+
+ //javascript Object
+  let pertion ={fName:"Suraya", lName:"Sarmin", age:23 };
+  console.log(pertion);
+
+  // The typeof Operator
+
+let q=" Showing of Type operators"
+console.log(typeof q);
+
+console.log(typeof " ", typeof 23,typeof true,typeof(3+4), typeof 3.45 );
+
+// un define
+ let volvo;
+ console.log(volvo)
+
+
+ // Empty Values
+  let value="";
+   console.log(value);
